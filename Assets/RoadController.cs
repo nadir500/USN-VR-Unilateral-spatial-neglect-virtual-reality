7f65ad1805c37a636d7d4b0444dc6c07e62280b7
ABD\Abdalla <abdalla0abu0amsha@gmail.com>
2017-12-19 13:35:33 +0200

    Generate road and putting the scene's basics
    Create the a road Dynamically

---
+++ Assets/RoadController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour {
+
+
+    public int numberOfPathsInSingleRoad = 4;
+    public GameObject sidewalk;
+    public GameObject streatPath;
+    public GameObject midWalk;
+    const int streatPathWidth = 10;
+    const float sidewalkWidth = 2.5f;
+    private Vector3 startPosition = new Vector3(10f, 0.1f, 0.0f);
+
+    // Use this for initialization
+    void Start () {
+        for(int i = 0; i < (numberOfPathsInSingleRoad / 2); i++)
+        {
+            Instantiate(streatPath, new Vector3(10f + (streatPathWidth * i), 0.1f, 0.0f), Quaternion.identity);
+        }
+        Instantiate(midWalk, new Vector3(6.25f + streatPathWidth * (numberOfPathsInSingleRoad / 2), 0.1f, 0.0f), Quaternion.identity);
+        for (int i = 0; i < (numberOfPathsInSingleRoad / 2); i++)
+        {
+            Instantiate(streatPath, new Vector3(12.5f + (streatPathWidth * i) + (streatPathWidth * (numberOfPathsInSingleRoad / 2)), 0.1f, 0.0f), Quaternion.identity);
+        }
+        Instantiate(midWalk, new Vector3(8.75f + streatPathWidth * (numberOfPathsInSingleRoad), 0.1f, 0.0f), Quaternion.identity);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
