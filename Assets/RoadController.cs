ad989befc579ff871a54c0f49c2f1e9c0cd62f12
Nadir Pervaiz <nadirpak95@hotmail.com>
2017-12-21 16:30:11 +0200

    VR Intial Update

--- Assets/RoadController.cs
+++ Assets/RoadController.cs
@@ -11,20 +11,20 @@ public class RoadController : MonoBehaviour {
     public GameObject midWalk;
     const int streatPathWidth = 10;
     const float sidewalkWidth = 2.5f;
-    private Vector3 startPosition = new Vector3(10f, 0.1f, 0.0f);
+    private Vector3 startPosition = new Vector3(10f, -2.0f, 0.0f);
 
     // Use this for initialization
     void Start () {
         for(int i = 0; i < (numberOfPathsInSingleRoad / 2); i++)
         {
-            Instantiate(streatPath, new Vector3(10f + (streatPathWidth * i), 0.1f, 0.0f), Quaternion.identity);
+            Instantiate(streatPath, new Vector3(10f + (streatPathWidth * i), -2.0f, 0.0f), Quaternion.identity);
         }
-        Instantiate(midWalk, new Vector3(6.25f + streatPathWidth * (numberOfPathsInSingleRoad / 2), 0.1f, 0.0f), Quaternion.identity);
+        Instantiate(midWalk, new Vector3(6.25f + streatPathWidth * (numberOfPathsInSingleRoad / 2), -2.0f, 0.0f), Quaternion.identity);
         for (int i = 0; i < (numberOfPathsInSingleRoad / 2); i++)
         {
-            Instantiate(streatPath, new Vector3(12.5f + (streatPathWidth * i) + (streatPathWidth * (numberOfPathsInSingleRoad / 2)), 0.1f, 0.0f), Quaternion.identity);
+            Instantiate(streatPath, new Vector3(12.5f + (streatPathWidth * i) + (streatPathWidth * (numberOfPathsInSingleRoad / 2)), -2.0f, 0.0f), Quaternion.identity);
         }
-        Instantiate(midWalk, new Vector3(8.75f + streatPathWidth * (numberOfPathsInSingleRoad), 0.1f, 0.0f), Quaternion.identity);
+        Instantiate(midWalk, new Vector3(8.75f + streatPathWidth * (numberOfPathsInSingleRoad), -2.0f, 0.0f), Quaternion.identity);
 
     }
 	
