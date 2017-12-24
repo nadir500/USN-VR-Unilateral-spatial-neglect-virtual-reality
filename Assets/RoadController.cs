30681f687ba4b559a4820cca8a2cb4427ce112f3
ABD\Abdalla <abdalla0abu0amsha@gmail.com>
2017-12-24 11:29:30 +0200

    Abd's Updates
    1- creating main menu for the game
    2- putting the parameters needed
    3- create animations for all guis
    4- create UIs Scripts

--- Assets/RoadController.cs
+++ Assets/RoadController.cs
@@ -11,20 +11,22 @@ public class RoadController : MonoBehaviour {
     public GameObject midWalk;
     const int streatPathWidth = 10;
     const float sidewalkWidth = 2.5f;
-    private Vector3 startPosition = new Vector3(10f, 0.1f, 0.0f);
+    private Vector3 startPosition = new Vector3(10f, -2.0f, 0.0f);
+    public GameObject BuildingsWrapper;
 
     // Use this for initialization
     void Start () {
         for(int i = 0; i < (numberOfPathsInSingleRoad / 2); i++)
         {
-            Instantiate(streatPath, new Vector3(10f + (streatPathWidth * i), 0.1f, 0.0f), Quaternion.identity);
+            Instantiate(streatPath, new Vector3(10f + (streatPathWidth * i), -2.0f, 0.0f), Quaternion.identity);
         }
-        Instantiate(midWalk, new Vector3(6.25f + streatPathWidth * (numberOfPathsInSingleRoad / 2), 0.1f, 0.0f), Quaternion.identity);
+        Instantiate(midWalk, new Vector3(6.25f + streatPathWidth * (numberOfPathsInSingleRoad / 2), -2.0f, 0.0f), Quaternion.identity);
         for (int i = 0; i < (numberOfPathsInSingleRoad / 2); i++)
         {
-            Instantiate(streatPath, new Vector3(12.5f + (streatPathWidth * i) + (streatPathWidth * (numberOfPathsInSingleRoad / 2)), 0.1f, 0.0f), Quaternion.identity);
+            Instantiate(streatPath, new Vector3(12.5f + (streatPathWidth * i) + (streatPathWidth * (numberOfPathsInSingleRoad / 2)), -2.0f, 0.0f), Quaternion.identity);
         }
-        Instantiate(midWalk, new Vector3(8.75f + streatPathWidth * (numberOfPathsInSingleRoad), 0.1f, 0.0f), Quaternion.identity);
+        Instantiate(midWalk, new Vector3(8.75f + streatPathWidth * (numberOfPathsInSingleRoad), -2.0f, 0.0f), Quaternion.identity);
+        BuildingsWrapper.transform.position = new Vector3(9.3f + streatPathWidth * (numberOfPathsInSingleRoad), 0, 0);
 
     }
 	
@@ -32,4 +34,9 @@ public class RoadController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void ButtonPressed()
+    {
+        Debug.Log("FUCK YOU FUCKEN BUTTON");
+    }
 }
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    private Animator mainMenuAnimator;
+    private bool active;
+    private Canvas myCanvas;
+
+    public GameObject settingsWrapper;
+    public GameObject creditsWrapper;
+
+    public Canvas uiMainCanvas;
+
+    // Use this for initialization
+    void Start () {
+        active = true;
+        myCanvas = this.gameObject.GetComponent<Canvas>();
+        mainMenuAnimator = this.gameObject.GetComponent<Animator>();
+        myCanvas.enabled = true;
+        
+	}
+    public void hide()
+    {
+        active = false;
+        mainMenuAnimator.SetBool("Active", active);
+    }
+    public void show()
+    {
+        active = true;
+        mainMenuAnimator.SetBool("Active", active);
+    }
+    public void newGame()
+    {
+        Debug.Log("newGame()");
+        uiMainCanvas.enabled = false;
+    }
+    public void loadGame()
+    {
+        Debug.Log("loadGame()");
+        uiMainCanvas.enabled = false;
+    }
+    public void settings()
+    {
+        Debug.Log("settings()");
+        this.hide();
+        settingsWrapper.GetComponent<Canvas>().enabled = true;
+        settingsWrapper.GetComponent<Animator>().SetBool("Active", true);
+    }
+    public void credits()
+    {
+        Debug.Log("credits()");
+        this.hide();
+        creditsWrapper.GetComponent<Canvas>().enabled = true;
+        creditsWrapper.GetComponent<Animator>().SetBool("Active", true);
+    }
+    public void exit()
+    {
+        Debug.Log("exit()");
+        Application.Quit();
+    }
+}
