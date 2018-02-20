5e20cd34f228e4408a833edffea4472cbe0ad84e
DESKTOP-8ABA1O1\Nadir Pervez <nadirpak95@hotmail.com>
2018-02-20 08:56:22 -0800

    Fixed the positions of the table to the camera added the camera events of leap motion still needs some fixes

---
+++ Assets/Template/UI/CalculatorController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CalculatorController : MonoBehaviour {
+
+	public Text numbersPad;
+	public Button leftButton;
+	public Button rightButton;
+
+	private string direction;
+
+
+	// Use this for initialization
+	void Start () {
+		numbersPad.text = "0";
+		leftButton.interactable = true;
+		rightButton.interactable = false;
+		direction = "none";
+	}
+	
+	public void putNumber(string number)
+	{
+		numbersPad.text = number;
+	}
+
+	public void Done()
+	{
+		Clear();
+		DeSelectDirection();
+	}
+	public void Clear()
+	{
+		numbersPad.text = "0";
+	}
+	public void SelectDirection(string direction)
+	{
+		this.direction = direction;
+		if(direction.Equals("left"))
+		{
+			leftButton.interactable = false;
+			rightButton.interactable = true;
+		}
+		else
+		{
+			leftButton.interactable = true;
+			rightButton.interactable = false;
+		}
+
+	}
+	public void DeSelectDirection()
+	{
+		this.direction = "none";
+		leftButton.interactable = true;
+		rightButton.interactable = true;
+	}
+
+}
