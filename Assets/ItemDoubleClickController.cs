b2d40bfd82ae6479676371b1c57a2d12c6a05b30
DESKTOP-8ABA1O1\Nadir Pervez <nadirpak95@hotmail.com>
2018-02-21 12:40:44 -0800

    added cdouble click on object to destroy added hold click on object to destroy TODO: Adding the DB Connection Function after recording the Hands clicking adding db connection function after finish the on pad rigestering

---
+++ Assets/ItemDoubleClickController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDoubleClickController : MonoBehaviour {
+
+
+	private int clickCount=0;
+	void OnTriggerExit(Collider hitFinger)
+	{
+		if (hitFinger.gameObject.name.Equals("bone3") && hitFinger.transform.parent.Equals("index"))
+		{
+			clickCount+=1;
+		}
+		if(clickCount==2)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+}
