d95116d89ef91f1e8f9945aa929927197a21f5de
ABD\Abdalla <abdalla0abu0amsha@gmail.com>
2018-02-22 09:26:10 +0200

    Add the table animation. Add tableObject class assigh the data to each table object

---
+++ Assets/Template/TableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TableObject : MonoBehaviour {
+
+    public TableController tableController;
+    public Transform canvas;
+    public string id;
+    public string level;
+    public string side;
+
+    void Start()
+    {
+        canvas = transform.Find("Canvas");
+    }
+    public void setValues(string id, string level, string side)
+    {
+        this.id = id;
+        this.level = level;
+        this.side = side;
+
+
+        if (side.Equals("Right"))
+        {
+            canvas.localEulerAngles = new Vector3(0, 270, 0);
+            canvas.GetChild(0).localScale = new Vector3(1, 1, 1);
+            canvas.Find("Canvas").GetChild(0).GetChild(0).localScale = new Vector3(-1, 1, 1);
+        }
+
+        transform.Find("Canvas").GetChild(0).GetChild(0).GetComponent<Text>().text = id.ToString();
+    }
+}
