9542592af3b4aea2a807c122bbbeba040138acfd
DESKTOP-8ABA1O1\Nadir Pervez <nadirpak95@hotmail.com>
2018-04-05 12:48:00 -0700

    some adjustments

--- Assets/Template/Controller/Cars/CarParentOnRoad.cs
+++ Assets/Template/Controller/Cars/CarParentOnRoad.cs
@@ -7,7 +7,7 @@ public class CarParentOnRoad : MonoBehaviour
 
     // This class aim to manage the generated road through adding it as a component 
     //in a "parent" after generating evey lane 
-   
+
     public GameObject[] carRefernces;  //array refernces to the childs (cars) in the parent attached with this class 
     public int index = 0; //index of the car we're trying to generate in every lane from the array above
     private CheckPointsController checkPointsController;  //helping us in detecting if the car hit the player or not 
@@ -17,24 +17,26 @@ public class CarParentOnRoad : MonoBehaviour
     {
         //intialize the array 
         carRefernces = new GameObject[this.transform.childCount];
-        carHandlerPool = new GameObject[ExperementParameters.lanes_per_direction * 10 *2];
+        carHandlerPool = new GameObject[ExperementParameters.lanes_per_direction * 10 * 2];
         //intialize chackpoint controller 
-        checkPointsController= GameObject.Find("CheckPointController").GetComponent<CheckPointsController>();
+        checkPointsController = GameObject.Find("CheckPointController").GetComponent<CheckPointsController>();
         for (int i = 0; i < carRefernces.Length; i++)  //reference every child to the array 
         {
             carRefernces[i] = this.transform.GetChild(i).gameObject;
         }
-        
-        if(this.gameObject.name.Split(' ')[1].Equals("1"))   //making some distance between each car in different lanes
-        InvokeRepeating("SpawnCar", 5, ExperementParameters.distanceBetweenCars);
+
+        if (this.gameObject.name.Split(' ')[1].Equals("1"))   //making some distance between each car in different lanes
+            InvokeRepeating("SpawnCar", 5, ExperementParameters.distanceBetweenCars);
         else
-         InvokeRepeating("SpawnCar",1, ExperementParameters.distanceBetweenCars);
+            InvokeRepeating("SpawnCar", 1, ExperementParameters.distanceBetweenCars);
     }
     //entering this method in a loop for spawning cars using setActive for effectiveness  
     void SpawnCar()
     {
-        if (index < carRefernces.Length &&!hitByCar)
+        
+        if (index < carRefernces.Length && !hitByCar)
         {
+        
             carRefernces[index].SetActive(true);
             index++;
         }
@@ -44,13 +46,11 @@ public class CarParentOnRoad : MonoBehaviour
             Debug.Log("Now respawn the first one (the car index is zero now)");
         }
 
-        if(RoadController.fadeout_after_crossing)
+        if (RoadController.fadeout_after_crossing)
         {
-            hitByCar=false;
-        
+            hitByCar = false;
         }
-     
-    }   
+    }
     //making all cars disappear except the one that hit the player SetActive(true)
     public void StopAllCarsAfterAccident(int carIndex)
     {
